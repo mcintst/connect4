651e7890cbb9e529d3e03c4a713f1ede06efcb27
Stuart McIntosh <mcintst@yahoo.com>
2015-04-03 09:39:46 +0800

    Initial Commit

---
+++ Connect4/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Connect4
+//
+//  Created by Stuart McIntosh on 3/4/15.
+//  Copyright (c) 2015 Stuart McIntosh. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
